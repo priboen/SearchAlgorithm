4b084053dd50e753070cc3bead9b0f4dfae80dd8
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-26 16:04:18 +0700

    adding variable and array

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace SearchAlgorithm
 {
     class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements of in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
         static void Main(string[] args)
         {
         }
