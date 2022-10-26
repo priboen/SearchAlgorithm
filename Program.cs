e8747690b41c9dc8bb39c10e81cb762ea8c6926b
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-26 16:10:45 +0700

    adding while true looping with the function in input method

--- Program.cs
+++ Program.cs
@@ -17,7 +17,17 @@ namespace SearchAlgorithm
 
         public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elements. \n");
 
+            }
         }
         static void Main(string[] args)
         {
